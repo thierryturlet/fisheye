7dfb9519b02ba075ce83aeadbe34b1ea56c7c8c1
thierry turlet <jawadgwada@hotmail.fr>
2025-01-09 19:28:15 +0200

    finalisation de la premiere page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -12,25 +12,41 @@ function displayPhotographers (photographers){
         const photographersContainer = document.getElementById('photographers');
     
             // Boucler sur chaque photographe
+
             photographers.forEach(photographer => {
-                // Créer un élément pour chaque photographe
-                const photographerElement = document.createElement('div');
-                photographerElement.classList.add('photographer');
+            // Créer un élément pour chaque photographe
+            const photographerElement = document.createElement('article');
+            photographerElement.classList.add('photographer');
            
-            // Ajouter une image si disponible
-            if (photographer.portrait) {
-                const imageElement = document.createElement('img');
-                imageElement.src = `./photos/Sample photos/Photographers ID Photos/${photographer.portrait}`;
-                photographerElement.appendChild(imageElement);
+            // Ajouter une image 
+           
+            const imageElement = document.createElement('img');
+            imageElement.src = `./photos/Sample photos/Photographers ID Photos/${photographer.portrait}`;
+            photographerElement.appendChild(imageElement);
 
             // Ajouter le nom du photographe
             const nameElement = document.createElement('h2');
             nameElement.textContent = photographer.name;
-            photographerElement.appendChild(nameElement);
+            photographerElement.appendChild(nameElement);         
+
+            // Ajout des villes et pays sur la même ligne
+            const locationElement = document.createElement('p');
+            locationElement.textContent = `${photographer.country}, ${photographer.city}`;
+            photographerElement.appendChild(locationElement);
+            locationElement.classList.add('photographer-location');
 
 
+             // Ajout des infos du photographe
+            const infoElement = document.createElement('p');
+            infoElement.textContent = photographer.tagline;
+            photographerElement.appendChild(infoElement)
+            infoElement.classList.add('photographer-tagline');
 
-            }
+            // Ajout des prix
+            const priceElement = document.createElement('p');
+            priceElement.textContent = `${photographer.price} €/jour`;
+            photographerElement.appendChild(priceElement)
+            priceElement.classList.add('photographer-price')
     
             // Ajouter l'élément au conteneur
             photographersContainer.appendChild(photographerElement);
