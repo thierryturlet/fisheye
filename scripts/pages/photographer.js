6ad51b444626703620c61c65a19ab497bf0d8840
thierry turlet <jawadgwada@hotmail.fr>
2025-01-19 21:37:45 +0200

    ajout else if pour les videos

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,26 +1,24 @@
 async function main() {
   const params = new URLSearchParams(window.location.search);
-  const  idFromUrl= params.get("id");
+  const idFromUrl = params.get("id");
   console.log(idFromUrl);
   let photographer = await getPhotographerById(idFromUrl);
 
-  let media = await getMediaPhotographer (idFromUrl);
+  let media = await getMediaPhotographer(idFromUrl);
   console.log(media);
 
-  
-  
-  displayPhotographerInfo(photographer)
-  displayMedia(media)
-
+  displayPhotographerInfo(photographer);
+  displayMedia(media);
 }
 
-
-
 function displayPhotographerInfo(photographer) {
-
   document.getElementById("name-container").innerHTML = photographer.name;
-  document.getElementById("infos-container").innerHTML = `${photographer.city}, ${photographer.country}`;
-  document.getElementById("photographer-portrait").src = `./photos/Sample photos/Photographers ID Photos/${photographer.portrait}`;
+  document.getElementById(
+    "infos-container"
+  ).innerHTML = `${photographer.city}, ${photographer.country}`;
+  document.getElementById(
+    "photographer-portrait"
+  ).src = `./photos/Sample photos/Photographers ID Photos/${photographer.portrait}`;
   document.getElementById("tagline-container").innerHTML = photographer.tagline;
 
   // ajout des class css
@@ -40,51 +38,76 @@ function displayPhotographerInfo(photographer) {
   taglineContainer.innerHTML = photographer.tagline;
   taglineContainer.classList.add("photographer-tagline");
 
-  console.log(photographer.name)
-  console.log(photographer.city,photographer.country)
-  console.log(`./photos/Sample photos/Photographers ID Photos/${photographer.portrait}`);
-  console.log(photographer.tagline)
-  console.log(photographer.portrait)
-
+  console.log(photographer.name);
+  console.log(photographer.city, photographer.country);
+  console.log(
+    `./photos/Sample photos/Photographers ID Photos/${photographer.portrait}`
+  );
+  console.log(photographer.tagline);
+  console.log(photographer.portrait);
 }
 
 function displayMedia(media) {
-
   // Boucler sur chaque media
   media.forEach((mediaItem) => {
-  const mediaContainer = document.getElementById("media-container");
-  console.log(mediaContainer)
-  
-  // Créer un élément pour chaque media
-  const mediaElement = document.createElement("article");
-  console.log(mediaElement)
-  
+    const mediaContainer = document.getElementById("media-container");
+    console.log(mediaContainer);
 
-  // Ajouter une image
-  const imageElement = document.createElement("img");
-  const photographerFolder = getPhotographerFolder(mediaItem.photographerId);
-  
-  imageElement.src =`./photos/imagesetvideos/${photographerFolder}/${mediaItem.image}`;
-  mediaElement.appendChild(imageElement)
-  console.log(imageElement)
-  
-  // Ajouter nom des images
-  const nomImage = document.createElement("p");
-  nomImage.textContent = mediaItem.title;
-  mediaContainer.appendChild(nomImage);
-  console.log(nomImage)
+    // Créer un élément pour chaque media
+    const mediaElement = document.createElement("article");
+    console.log(mediaElement);
 
-  
+    let imageElement;
 
-  // Ajouter l'image à l'élément media
-  mediaElement.appendChild(imageElement);
-  mediaElement.appendChild(nomImage);
+    if (mediaItem.image) {
+      // Ajouter une image
+      imageElement = document.createElement("img"); // Initialiser imageElement ici
+      const photographerFolder = getPhotographerFolder(
+        mediaItem.photographerId
+      );
 
-  // Ajouter l'élément média au conteneur principal
-  mediaContainer.appendChild(mediaElement)
-  }
-)
+      imageElement.src = `./photos/imagesetvideos/${photographerFolder}/${mediaItem.image}`;
+      mediaElement.appendChild(imageElement);
+      console.log(imageElement);
+    
+
+    // Ajouter nom des images
+    const nomImage = document.createElement("p");
+    nomImage.textContent = mediaItem.title;
+    mediaElement.appendChild(nomImage); // Ajouter le nom à l'intérieur de mediaElement
+    console.log(nomImage);
+    }
+
+    else if (mediaItem.video) {
+
+      // Ajouter une video
+      let videoElement;
+      videoElement = document.createElement("video");
+      const photographerFolder = getPhotographerFolder(
+        mediaItem.photographerId
+      );
+
+      videoElement.src = `./photos/imagesetvideos/${photographerFolder}/${mediaItem.video}`;
+      mediaElement.appendChild(videoElement);
+      console.log(videoElement);
+
+      // Ajouter nom des video
+    const nomVideo = document.createElement("p");
+    nomVideo.textContent = mediaItem.title;
+    mediaElement.appendChild(nomVideo); // Ajouter le nom à l'intérieur de mediaElement
+    console.log(nomVideo);
+    
+    }
+
+    // Ajouter l'élément média au conteneur principal
+    mediaContainer.appendChild(mediaElement);
+
+
+  });
 }
+
+
+
 function getPhotographerFolder(photographerId) {
   // Retourner le nom du dossier photographe
   return photographerId;
