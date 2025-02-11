a3b5a3620857c1176aab7d914b9599b09532e430
thierry turlet <jawadgwada@hotmail.fr>
2025-02-11 20:28:50 +0200

    recuperattion et soumission des inputs

--- scripts/utils/contactForms.js
+++ scripts/utils/contactForms.js
@@ -1,9 +1,28 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+	modal.style.display = "flex";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+let form = document.querySelector("form");
+console.log(form);
+form.addEventListener("submit",(event) => {
+    event.preventDefault();
+    
+
+let inputPrenom = document.getElementById("prenom").value;
+let inputNom = document.getElementById("nom").value;
+let inputEmail = document.getElementById("email").value;
+let inputMessage = document.getElementById("message").value;
+
+
+
+console.log("Prénom :", inputPrenom);
+    console.log("Nom :", inputNom);
+    console.log("Email :", inputEmail);
+    console.log("Message :", inputMessage);
+});
